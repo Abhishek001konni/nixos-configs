fc30a4c06951d3adf1cf94cfdea1cd5425f02a1a
Abhishek001konni <abhishek001konni@gmail.com>
2025-03-06 20:07:57 -0500

    remove allowUnfree from home

--- home/home.nix
+++ home/home.nix
@@ -4,8 +4,6 @@
   home.username = "abhishek";
   home.homeDirectory = "/home/abhishek";
   
-  nixpkgs.config.allowUnfree = true;
-
   # This value determines the Home Manager release that your
   # configuration is compatible with. This helps avoid breakage
   # when a new Home Manager release introduces backwards
