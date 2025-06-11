05f19f305820edbf6e0138c3e34ee190990b6761
Abhishek001konni <abhishek001konni@gmail.com>
2025-06-10 21:28:27 -0400

    change gtk theme to mocha

--- home/gtk.nix
+++ home/gtk.nix
@@ -3,27 +3,34 @@
   pkgs,
   ...
 }:
+let
+  catppuccinVariant = "mocha";
+  catppuccinAccent = "blue";
+  catppuccinSize = "standard";
 
+  catppuccinGtk = pkgs.catppuccin-gtk.override {
+    variant = catppuccinVariant;
+    accents = [ catppuccinAccent ];
+    size = catppuccinSize;
+  };
+in
 {
   gtk = {
     enable = true;
     font.name = "Inter 10";
     theme = {
-      name = "catppuccin-frappe-blue-standard";
-      package = pkgs.catppuccin-gtk;
+      name = "catppuccin-${catppuccinVariant}-${catppuccinAccent}-${catppuccinSize}";
+      package = catppuccinGtk;
     };
     iconTheme = {
       name = "Papirus-Dark";
       package = pkgs.papirus-icon-theme;
     };
-
     gtk3.extraConfig = {
       "gtk-application-prefer-dark-theme" = "1";
     };
-
     gtk4.extraConfig = {
       "gtk-application-prefer-dark-theme" = "1";
     };
-
   };
 }
